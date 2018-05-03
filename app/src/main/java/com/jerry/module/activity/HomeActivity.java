6d7259dfc3a5da8da78ad09979a05d96ea363be3
JerryTan <thrforever@126.com>
2018-05-03 14:18:41 +0800

    2-1 首页框架-搭建铺垫

---
+++ app/src/main/java/com/jerry/module/activity/HomeActivity.java
@@ -0,0 +1,15 @@
+package com.jerry.module.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.jerry.module.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
